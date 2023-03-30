a6caa7445b6de3ab07d8efae27c9eeb3c14d3f00
aabhushanadhikari <aaabhushan10@gmail.com>
2023-03-30 16:48:07 +0545

    while loop

---
+++ src/flow_control/While_Loop.java
@@ -0,0 +1,18 @@
+package flow_control;
+
+import java.util.Scanner;
+
+public class While_Loop {
+	public static void main(String[] args) {
+		int i,rev=0,r;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number you want ot reverse");
+		i=sc.nextInt();
+		while(i!=0) {
+		r=i%10;
+		rev=10*rev+r;
+		i=i/10;
+		}
+		System.out.println("The number reversed is "+ rev);
+	}
+}
